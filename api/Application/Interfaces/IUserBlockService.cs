cd6dee2b3cad7da967afd76333c84c74375cea6b
Денис Петров <ipz224_pdv@student.ztu.edu.ua>
2026-05-22 20:58:53 +0300

    feat: add favicon and update HTML title for improved branding

--- api/Application/Interfaces/IUserBlockService.cs
+++ api/Application/Interfaces/IUserBlockService.cs
@@ -7,6 +7,7 @@ public interface IUserBlockService
     Task BlockAsync(Guid blockerId, Guid blockedUserId);
     Task UnblockAsync(Guid blockerId, Guid blockedUserId);
     Task<bool> IsBlockedAsync(Guid blockerId, Guid blockedUserId);
+    Task<bool> IsBlockedByAsync(Guid userId, Guid potentialBlockerId);
     Task<IEnumerable<Guid>> GetBlockedUserIdsAsync(Guid blockerId);
     Task<IEnumerable<BlockedUserDto>> GetBlockedUsersWithDetailsAsync(Guid blockerId);
 }
